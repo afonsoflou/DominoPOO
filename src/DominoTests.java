9b9e236fedee61af78cbd2832f7197590cbf3fb7
afons <a71251@ualg.pt>
2022-04-23 15:02:05 +0100

    bla blau

--- src/DominoTests.java
+++ src/DominoTests.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class DominoTests{
 
+    Domino other = new Domino(1,1);
+
     @Test
     public void constructorTest(){
         assertEquals(new Domino(1,2).getX(),1);
@@ -17,6 +19,39 @@ public class DominoTests{
         assertEquals(new Domino(3,2).getValue(),5);
         assertEquals(new Domino(5,6).getValue(),11);
     }
+
+    @Test
+    public void isDoubleTest(){
+        assertTrue(new Domino(2, 2).isDouble());
+        assertFalse(new Domino(1, 2).isDouble());
+        assertTrue(new Domino(3, 3).isDouble());
+        assertFalse(new Domino(1, 3).isDouble());
+    }
+
+    @Test
+    public void isEqualTest(){
+        assertTrue(new Domino(1, 1).isEqual(other));
+        assertFalse(new Domino(1, 2).isEqual(other));
+        assertFalse(new Domino(1, 3).isEqual(other));
+        assertFalse(new Domino(1, 4).isEqual(other));
+    }
+
+    @Test
+    public void canConnectTest(){
+        assertTrue(new Domino(1,3).canConnect(other));
+        assertTrue(new Domino(1,2).canConnect(other));
+        assertFalse(new Domino(2,3).canConnect(other));
+        assertFalse(new Domino(3,3).canConnect(other));
+    }
+
+    @Test
+    public void isStarterTest(){
+        assertTrue(new Domino(6,6).isStarter());
+        assertFalse(new Domino(5,6).isStarter());
+        assertFalse(new Domino(4,6).isStarter());
+        assertFalse(new Domino(3,1).isStarter());
+    }
+
 
 
 }
--- src/PlayerTests.java
+++ src/PlayerTests.java
@@ -1,4 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class PlayerTests {
+
+    public void constructorTest(){
+
+    }
 
 }
 //
