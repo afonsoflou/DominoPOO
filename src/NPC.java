d602f47dc3761f9c2aa94e6da169e60ca8a154fb
Ohm <ohmmsons@gmail.com>
2022-04-23 17:21:56 +0100

    Human Fixes

--- src/NPC.java
+++ src/NPC.java
@@ -5,16 +5,9 @@ public class NPC extends Player{
         super(board, PlayerName, dominoes);
     }
 
-    private void printPlayablePieces(){
-        for(Domino domino : dominoes)
-            for(Domino corner : board.getCorners())
-                if(domino.canConnect(corner))
-                    domino.print();
-    }
-
 
     public void play() {
         if(!canPlay()) return;
-        printPlayablePieces();
+
     }
 }
