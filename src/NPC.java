41a8e96401c6dc564edf958011f08fc0f580f3c5
Ohm <ohmmsons@gmail.com>
2022-04-25 10:24:57 +0100

    Fixed Quevin Incident Fallout

--- src/NPC.java
+++ src/NPC.java
@@ -1,4 +1,6 @@
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class NPC extends Player{
@@ -9,5 +11,26 @@ public class NPC extends Player{
 
     public void play() {
         if(!canPlay()) return;
+
+        LinkedList<Domino> playableDominoes = new LinkedList<>();
+
+        for(Domino domino : dominoes)
+            for(Domino corner : gameLine.getCorners())
+                if(domino.canConnect(corner))
+                    playableDominoes.add(domino);
+
+        Collections.shuffle(playableDominoes);
+        Domino playedDomino = playableDominoes.remove();
+        dominoes.removeIf(x -> x.isEqual(playedDomino));
+
+        Domino playedCorner = null;
+
+        for(Domino corner : gameLine.getCorners())
+            if(corner.canConnect(playedDomino)) {
+                playedCorner = corner;
+                break;
+            }
+
+        gameLine.insertDomino(playedDomino,playedCorner);
     }
 }
