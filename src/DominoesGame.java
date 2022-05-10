8b0f0bbe349a85ca63c2d08a7ee3f8206c62db90
comfypad <quevinp@protonmail.com>
2022-05-10 10:22:58 +0100

    GameBoard unit tests, and a rudimentary AI implementation, might use weights on the future and some kind of deep learning device, of course with 3 weights it won't be nearly as complicated or expensive to compute.

--- src/DominoesGame.java
+++ src/DominoesGame.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.*;
 
 public class DominoesGame {
@@ -5,12 +8,101 @@ public class DominoesGame {
     private final int nLines;
     private final int nColumns;
     private final Player[] players;
+    public static final PrintStream out = new PrintStream(new OutputStream() {
+        @Override
+        public void write(int i) throws IOException {
+            return;
+        }
+    });
+
+
+    //There is a bug were a domino will be put on top of another one, although this is so unlikely it happens on around 1/4 of the times.
+    //Considering the main runs the games 1000 times. It is indeed unlikely. I will patch it on the future.
+    public static void main(String[] argv){
+       var game = new DominoesGame(40,15);
+       int nTESTS = 1000;
+       int[] result = new int[4];
+        for(int i =0; i < nTESTS; i++)
+           result[game.testAi()]++;
+        System.out.println("1st place :" + result[0] + ", 2nd place :" + result[1] + ", 3rd place :" + result[2] + ", 4th place :" + result[3]);
+    }
+
 
 
     public DominoesGame(int nLines, int nColumns) {
         this.nLines = nLines;
         this.nColumns = nColumns;
         this.players = new Player[4];
+    }
+
+    //true if it wins false otherwise.
+    public int testAi() {
+
+        var stdout = System.out;
+       System.setOut(out);
+
+        //Create Board
+        GameBoard board = new GameBoard(nColumns, nLines);
+        GameLine gameLine = new GameLine(board);
+
+        //Create Dominoes and shuffle them
+        LinkedList<Domino> Dominoes = new LinkedList<>();
+        for(int i = 0; i < 7; i++)
+            for(int j = i; j < 7; j++)
+                Dominoes.add(new Domino(i, j));
+        Collections.shuffle(Dominoes);
+
+        //Create AI Player
+        this.players[0] = new AI(gameLine, "AI", Dominoes.subList(0, 7), board);
+
+        //Create NPC players
+        for(int i = 1; i <= 3; i++)
+            this.players[i] = new NPC(gameLine, "NPC " + i, Dominoes.subList(i * 7, (i + 1) * 7), board);
+
+        //Find first player
+        int currentPlayer = 0;
+        for(Player player : players) {
+            if(player.isFirst()) {
+                //First Player plays
+                System.out.println(players[currentPlayer % 4].getName() + "'s turn");
+                players[currentPlayer % 4].play();
+                //board.print();
+                break;
+            }
+            currentPlayer++;
+        }
+
+        //Players play until there is a winner, didntPlay counts how many players can't play in the turn
+        //If the counter reaches 4, the game ends since no one can play.
+        int didntPlay = 0;
+        while(didntPlay < 4) {
+            if(players[currentPlayer % 4].isWinner()) break;
+            currentPlayer++;
+            System.out.println();
+            System.out.println(players[currentPlayer % 4].getName() + "'s turn");
+            System.out.println();
+            if(players[currentPlayer % 4].canPlay()) {
+                players[currentPlayer % 4].play();
+                didntPlay = 0;
+                //board.print();
+            } else {
+                System.out.println("Can't Play");
+                didntPlay++;
+            }
+        }
+
+        //Put winner at start of array
+        Player swap = players[0];
+        players[0] = players[currentPlayer % 4];
+        players[currentPlayer % 4] = swap;
+
+        //Sort players by points at the end;
+        Arrays.sort(players, Comparator.comparingInt(Player::getPoints));
+        System.setOut(stdout);
+        for(int i = 0;i < 4 ;i++)
+            if(players[i] instanceof AI)
+                return i;
+        throw new IllegalArgumentException("What happened");
     }
 
     public void startGame() {
@@ -77,7 +169,6 @@ public class DominoesGame {
 
         //Print results
         for(int i = 0; i<4; i++) System.out.println(i+1 + " Place : "+ players[i].getName() + "- " + players[i].getPoints() + " Points");
-
     }
 
 }
--- src/GameBoardTests.java
+++ src/GameBoardTests.java
@@ -1,15 +1,163 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameBoardTests {
+    Domino verticalDomino = new Domino(2,6);
+    Domino verticalDoubleDomino = new Domino(6,6);
+    Domino horizontalDomino = new Domino(1,6);
+    Domino horizontalDoubleDomino = new Domino(2,2);
 
-
-
-    //don't know how to test this, suffers from the same problem that the player suffers
-    //I mean I know how to test this, but the time and effort put seem largely worthless when
-    //a simple human check is much more efficient and accurate.
-    @Test
-    public void testInsertPiece(){
-
+    {
+        verticalDomino.beVertical();
+        verticalDoubleDomino.beVertical();
     }
 
+    /*vertical domino and vertical double domino board representation
+                  x
+                  -
+                  x
+     horizontal domino board representation
+                  xx
+     horizontal double domino board representation
+                  x-x
+     */
+
+
+    //lab 3 unit test code.
+    // Redirect STDIN and STDOUT for Mooshak like black box tests
+    static private ByteArrayOutputStream setIOStreams(String input) {
+        //set stdin
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        //set stdout
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(os);
+        System.setOut(ps);
+        return os;
+    }
+
+    //the board must at least ensure that 100 spaces are available to play on the board at the time of creation.
+    @Test
+    public void testGameBoard(){
+       assertThrows(IllegalArgumentException.class,() -> new GameBoard(0,0));
+       assertThrows(IllegalArgumentException.class,() -> new GameBoard(10,9));
+       assertDoesNotThrow(() -> new GameBoard(10,10));
+    }
+
+    //acceptable range for a board (25,25), for horizontal domino (0 to 23,0 to24), for vertical domino (0 to 24,2 to 24)
+    //for horizontal doubles (0 to 22,0 to 24) and for the vertical doubles it should be identical to the vertical domino.
+    @Test
+    public void testInsertDomino(){
+        GameBoard gameBoard = new GameBoard(25,25);
+
+        //vertical domino check
+        assertThrows(IllegalArgumentException.class,() -> gameBoard.insertDomino(0,1, verticalDomino));
+        assertDoesNotThrow(() -> gameBoard.insertDomino(0,2, verticalDomino));
+        assertDoesNotThrow(() -> gameBoard.insertDomino(24,24,verticalDomino));
+
+
+        GameBoard gameBoard1 = new GameBoard(25,25);
+        //horizontal domino check
+        assertThrows(IllegalArgumentException.class,() -> gameBoard1.insertDomino(24,0,horizontalDomino));
+        assertDoesNotThrow(() -> gameBoard1.insertDomino(23,0,horizontalDomino));
+        assertDoesNotThrow(() -> gameBoard1.insertDomino(23,24,horizontalDomino));
+
+        GameBoard gameBoard2 = new GameBoard(25,25);
+        //vertical double domino check.
+        assertThrows(IllegalArgumentException.class,() -> gameBoard2.insertDomino(0,1, verticalDoubleDomino));
+        assertDoesNotThrow(() -> gameBoard2.insertDomino(0,2, verticalDoubleDomino));
+        assertDoesNotThrow(() -> gameBoard2.insertDomino(24,24,verticalDoubleDomino));
+
+        GameBoard gameBoard3 = new GameBoard(25,25);
+        //horizontal double domino check
+        assertThrows(IllegalArgumentException.class,() -> gameBoard3.insertDomino(23,0,horizontalDoubleDomino));
+        assertDoesNotThrow(() -> gameBoard3.insertDomino(22,0,horizontalDoubleDomino));
+        assertDoesNotThrow(() -> gameBoard3.insertDomino(22,24,horizontalDoubleDomino));
+
+        //cannot insert pieces on top of another pieces.
+        GameBoard gameBoard4 = new GameBoard(25,25);
+        gameBoard4.insertDomino(10,10,verticalDomino);
+
+        assertThrows(IllegalArgumentException.class,() -> gameBoard4.insertDomino(10,10,verticalDomino));
+        assertThrows(IllegalArgumentException.class,() -> gameBoard4.insertDomino(9,8,horizontalDomino));
+        assertThrows(IllegalArgumentException.class,() -> gameBoard4.insertDomino(8,8,horizontalDoubleDomino));
+
+        gameBoard4.insertDomino(10,2,verticalDomino);
+        assertThrows(IllegalArgumentException.class,() -> gameBoard4.insertDomino(10,4,verticalDomino));
+        assertThrows(IllegalArgumentException.class,() -> gameBoard4.insertDomino(10,4,verticalDoubleDomino));
+    }
+
+    @Test
+    void printTest() throws IOException {
+
+        var gameBoard = new GameBoard(30,10);
+
+        //insert double vertical 66 on coordinates 10,4.
+        String expected = new String(
+                "#".repeat(32) + "\n" +
+                        ("#" + " ".repeat(30) + "#\n").repeat(5) +
+                        "#" + " ".repeat(10) + "6" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(10) + "-" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(10) + "6" + " ".repeat(19) + "#\n" +
+                        ("#" + " ".repeat(30) + "#\n").repeat(2) +
+                        "#".repeat(32) + "\n"
+        );
+
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream os = setIOStreams("");
+        gameBoard.insertDomino(10,4,verticalDoubleDomino);
+        gameBoard.print();
+
+/* in case you want to see a comparison between the output and the expected output put this after gameBoard.print()
+        System.setOut(stdout);
+        System.out.println("expected\n" + expected);
+        System.out.println("received\n" + os.toString());
+*/
+
+        assertEquals(expected,os.toString());
+        os.close();
+
+
+        expected = new String(
+                "#".repeat(32) + "\n" +
+                        ("#" + " ".repeat(30) + "#\n").repeat(5) +
+                        "#" + " ".repeat(7) + "   6" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(7) + "16 -" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(7) + "   6" + " ".repeat(19) + "#\n" +
+                        ("#" + " ".repeat(30) + "#\n").repeat(2) +
+                        "#".repeat(32) + "\n"
+        );
+
+        os = setIOStreams("");
+        gameBoard.insertDomino(7,3,horizontalDomino);
+        gameBoard.print();
+
+        assertEquals(expected,os.toString());
+        os.close();
+
+        expected = new String(
+                "#".repeat(32) + "\n" +
+                        "#" + " ".repeat(7) + "  2-2" + " ".repeat(18) + "#\n" +
+                        "#" + " ".repeat(7) + "   2" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(7) + "   -" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(7) + "   6" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(30) + "#\n" +
+                        "#" + " ".repeat(7) + "   6" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(7) + "16 -" + " ".repeat(19) + "#\n" +
+                        "#" + " ".repeat(7) + "   6" + " ".repeat(19) + "#\n" +
+                        ("#" + " ".repeat(30) + "#\n").repeat(2) +
+                        "#".repeat(32) + "\n"
+        );
+
+        os = setIOStreams("");
+        gameBoard.insertDomino(10,8,verticalDomino);
+        gameBoard.insertDomino(9,9,horizontalDoubleDomino);
+        gameBoard.print();
+
+
+        assertEquals(expected,os.toString());
+        os.close();
+    }
 }
